06d3f13b2e6620b6377996f2c72cd3625d2b8082
楚浩远 <>
2019-05-05 19:43:06 +0800

    shader 学习

--- ShaderHelper/assets/shader/TestShader.js
+++ ShaderHelper/assets/shader/TestShader.js
@@ -1,25 +1,37 @@
 const renderEngine = cc.renderer.renderEngine;
 const renderer = renderEngine.renderer;
 const CustomMaterial = require('CustomMaterial');
+let size = {
+    width: cc.view.getVisibleSize().width / cc.view.getDevicePixelRatio(),
+    height: cc.view.getVisibleSize().height / cc.view.getDevicePixelRatio()
+}
 const shader = {
     name: 'TestShader',
     params: [
-        { name: 'time', type: renderer.PARAM_FLOAT, default: 0 }
+        { name: 'time', type: renderer.PARAM_FLOAT, default: 0 },
+        { name: 'size', type: renderer.PARAM_FLOAT2, default: [360, 640] }
     ],
-    start() {
+    start(sprite, material) {
         this._start = Date.now();
         this._notTime = 0;
+
+        console.log("size", size, cc.view.getDevicePixelRatio());
+        material.setParamValue('size', [360, 640]);
     },
-    update(sprite, material) {
-        const now = Date.now();
-        let time = (now - this._start) / 1000;
+    update(sprite, material, dt) {
         // if (time >= 1) {
         //     time = 0;
         //     this._start = now;
         // }
+        this._notTime += dt;
+        if (this._notTime > 1) {
+            this._notTime = 0;
+        }
 
-        material.setParamValue('time', Math.sin(time) * 0.1 + 0.05);
+        material.setParamValue('time', this._notTime * 2 - 1);
+        material.setParamValue('size', [360, 640]);
 
+        // material.setParamValue('size', [200,200]);
         // if (this._notTime !== undefined){
         //     this._notTime ++;
         //     if (this._notTime > 10){
@@ -27,7 +39,7 @@ const shader = {
         //         material.setParamValue('time', time);
         //     }
         // }
-        
+
     },
     defines: [],
     vert: `
@@ -47,17 +59,34 @@ const shader = {
         uniform sampler2D texture;
         uniform vec4 color;
         uniform float time;
-        varying vec2 uv0;
-        void main(){
-            vec4 src_color = color * texture2D(texture, uv0).rgba;
-            float width = 0.03;
-            float start = tan(time);
-            if (uv0.x >  start && uv0.x < start + width){
-                gl_FragColor = vec4(src_color.r + 0.4, src_color.g + 0.4, src_color.b + 0.4, 1);
-            }else {
-                gl_FragColor = src_color;
+        uniform vec2 size;
 
-            }
+        varying vec2 uv0;
+        
+        
+        void main(){
+
+
+            // vec4 color = 
+            // vec2 coordPos = vec2(gl_FragCoord.x / 360.0 * 2.0 - 1.0, gl_FragCoord.y / 640.0 * 2.0 - 1.0);
+
+
+            // float ss = sin(time  + coordPos.y);
+            // coordPos *= ss;
+
+
+            // coordPos.y = coordPos.y - 0.25;
+            // float r = length(coordPos);
+            // float a = atan(coordPos.x, coordPos.y) / 3.1415926;
+            // float h = abs(a);
+            // float d = (13.0*h - 22.0*h*h + 10.0*h*h*h)/(6.0-5.0*h);
+            // vec3 bgColor = vec3(1.0, 1.0, 1.0);
+            // vec3 redColor = vec3(1.0, 0.0, 0.0);
+            // float s = smoothstep(0.0, 0.1, d - r);
+            // float c = clamp( s   , 0.0, 1.0);
+            // vec3 endColor = mix(bgColor, redColor, s);
+            // gl_FragColor = vec4(endColor.xyz, 1.0);
+
         }
     `,
 }
